e862fba50b2bdab8dfd6a2b4c5d0593583fe1e6b
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-11-27 17:43:37 +0530

    add increment endpoint with global count variable protected by mutex

--- go_backend/main.go
+++ go_backend/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	address := ":" + port
+
+	globalCount := GloblaCount{}
+
+	http.HandleFunc("/increment", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			globalCount.mu.Lock()
+			defer globalCount.mu.Unlock()
+			globalCount.value++
+			w.Write(globalCount.byteValue())
+		}
+	})
+	log.Printf("Server started on port: %v\n", port)
+	log.Fatal(http.ListenAndServe(address, nil))
 }
